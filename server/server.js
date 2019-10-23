b882768c0fecd6cff12a92ffbbba78650b298fdb
nishat1 <nishatgupta1@gmail.com>
2019-10-22 18:33:31 -0700

    Added employees API

--- server/server.js
+++ server/server.js
@@ -9,6 +9,7 @@ const db = require('./db');
 app.use(bodyParser.json());
 
 app.use('/api/stores', require('./routes/api/stores'));
+app.use('/api/employees', require('./routes/api/employees'));
 app.use('/', require('./routes/api/items'));
 app.use('/', require('./routes/api/internal'));
 app.use('/', require('./routes/api/users'));
