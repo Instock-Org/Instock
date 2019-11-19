5a58b43f8063dd58d6a0788ad02d6149923f727e
nishat1 <nishatgupta1@gmail.com>
2019-11-19 15:36:22 -0800

    Mocking db connect calls

---
+++ server/__mocks__/db.js
@@ -0,0 +1,17 @@
+const connect = (cb) => {
+    cb()
+};
+
+const getPrimaryKey = (_id) => {
+    return _id;
+};
+
+const getDB = () => {
+    return null;
+};
+
+module.exports = {
+    getDB,
+    connect,
+    getPrimaryKey
+};
