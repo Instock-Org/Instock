40761f1c834a1ed1a8d20619664521109e0287ea
Eric Duong <ericduong515@gmail.com>
2019-11-19 01:52:49 -0800

    Adding complex logic too

--- server/routes/api/stores.js
+++ server/routes/api/stores.js
@@ -11,10 +11,72 @@ const maps = require("./maps");
 
 const redisClient = require("redis").createClient;
 const redis = redisClient(constants.REDIS_PORT, "localhost");
+const dbHelper = require("../../apiDbHelperStores");
 
 /**
  * GET requests
  */
+
+const complexLogic = async (req, res) => {
+    let shoppingList = req.body.shoppingList || []; 
+    const latitude = req.body.location ? req.body.location.latitude : constants.DEFAULT_LATITUDE;
+    const longitude = req.body.location ? req.body.location.longitude : constants.DEFAULT_LONGITUDE;
+    const radiusKm = req.body.radius || constants.DEFAULT_RADIUS_KM;
+
+    // Input checking
+    if (shoppingList.length === 0) {
+        // TODO: Change to a different response
+        res.sendStatus(constants.RES_NOT_FOUND);
+        return;
+    }
+
+    if (latitude > constants.MAX_LATITUDE || latitude < constants.MIN_LATITUDE ||  longitude > constants.MAX_LONGITUDE || longitude < constants.MIN_LONGITUDE) {
+        res.sendStatus(constants.RES_BAD_REQUEST);
+        return;
+    }
+
+    // Mutate the array to help out with caching so that shopping list won't be redundant
+    shoppingList.sort();
+    shoppingList = shoppingList.map((keyword) => keyword.toLowerCase());
+    
+    // This combination of inputs will be used for Redis to identify if it has been searched before
+    const redisKey = JSON.stringify({
+        shoppingList,
+        latitude,
+        longitude,
+        radiusKm
+    });
+
+    redis.get(redisKey, (rediserr, reply) => {
+        if (rediserr) {
+            res.status(constants.RES_INTERNAL_ERR);
+            return;
+        }
+        else if (reply) {
+            res.status(constants.RES_OK).send(JSON.parse(reply)); // exists in cache
+            return;
+        }
+        else {
+            // Calculate long/lat bounds (north, south, west, east)
+            // Will assume square instead of circle
+            const eastBoundaryLong = longitude + (radiusKm / constants.R_EARTH) * (180.0 / Math.PI) / Math.cos(latitude * Math.PI/180.0);
+            const westBoundaryLong = longitude - (radiusKm / constants.R_EARTH) * (180.0 / Math.PI) / Math.cos(latitude * Math.PI/180.0);
+            const northBoundaryLat = latitude + (radiusKm / constants.R_EARTH) * (180.0 / Math.PI);
+            const southBoundaryLat = latitude - (radiusKm / constants.R_EARTH) * (180.0 / Math.PI);
+
+            var storePickupList = [];
+
+            // Convert each item on shopping list to regex to make it case insensitive
+            shoppingList.forEach((searchTerm, key) => {
+                shoppingList[key] = new RegExp(searchTerm, "i");
+            });
+
+            // Get item IDs by name
+            dbHelper.complexLogic(shoppingList, res);
+        }
+    });
+    return;
+};
 
 // Get all stores and their details. 
 router.get("/", (req, res) => {
@@ -76,177 +138,7 @@ router.get("/:storeID", (req, res) => {
 
 // Given a shopping list and user's location, find all stores nearby that have the items on the shopping list in stock
 // ASSUMES NO AMBIGUITY ON ITEMS: EXACT MATCH ONLY (NOT CASE SENSITIVE)
-router.post("/feweststores", (req, res) => {
-    var shoppingList = req.body.shoppingList || []; 
-    const latitude = req.body.location ? req.body.location.latitude : constants.DEFAULT_LATITUDE;
-    const longitude = req.body.location ? req.body.location.longitude : constants.DEFAULT_LONGITUDE;
-    const radiusKm = req.body.radius || constants.DEFAULT_RADIUS_KM;
-
-    // Input checking
-    if (shoppingList.length === 0) {
-        res.sendStatus(constants.RES_NOT_FOUND);
-        return;
-    }
-
-    if (latitude > constants.MAX_LATITUDE || latitude < constants.MIN_LATITUDE ||  longitude > constants.MAX_LONGITUDE || longitude < constants.MIN_LONGITUDE) {
-        res.sendStatus(constants.RES_BAD_REQUEST);
-        return;
-    }
-
-    // Mutate the array to help out with caching so that shopping list won't be redundant
-    shoppingList.sort();
-    shoppingList = shoppingList.map((keyword) => keyword.toLowerCase());
-    
-    // This combination of inputs will be used for Redis to identify if it has been searched before
-    const redisKey = JSON.stringify({
-        shoppingList,
-        latitude,
-        longitude,
-        radiusKm
-    });
-
-    redis.get(redisKey, (rediserr, reply) => {
-        if (rediserr) {
-            res.status(constants.RES_INTERNAL_ERR);
-            return;
-        }
-        else if (reply) {
-            res.status(constants.RES_OK).send(JSON.parse(reply)); // exists in cache
-            return;
-        }
-        else {
-            // Calculate long/lat bounds (north, south, west, east)
-            // Will assume square instead of circle
-            const eastBoundaryLong = longitude + (radiusKm / constants.R_EARTH) * (180.0 / Math.PI) / Math.cos(latitude * Math.PI/180.0);
-            const westBoundaryLong = longitude - (radiusKm / constants.R_EARTH) * (180.0 / Math.PI) / Math.cos(latitude * Math.PI/180.0);
-            const northBoundaryLat = latitude + (radiusKm / constants.R_EARTH) * (180.0 / Math.PI);
-            const southBoundaryLat = latitude - (radiusKm / constants.R_EARTH) * (180.0 / Math.PI);
-
-            var storePickupList = [];
-
-            // Convert each item on shopping list to regex to make it case insensitive
-            shoppingList.forEach((searchTerm, key) => {
-                shoppingList[key] = new RegExp(searchTerm, "i");
-            });
-
-            // Get item IDs by name
-            db.getDB().collection(constants.COLLECTION_ITEMS).find({
-                "name": { $in: shoppingList }
-            }).toArray((itemErr, items) => {
-                // Get nearby stores
-                if (items.length === 0) {
-                    res.sendStatus(constants.RES_NOT_FOUND);
-                    return;
-                }
-                var itemIds = items.map((item) => item._id);
-                db.getDB().collection(collection).find({
-                    "lat": {
-                        $lt: northBoundaryLat,
-                        $gt: southBoundaryLat
-                    },
-                    "lng": {
-                        $lt: eastBoundaryLong,
-                        $gt: westBoundaryLong
-                    }
-                }).toArray((storeErr, stores) => {
-                    if (stores.length === 0) {
-                        res.sendStatus(constants.RES_NOT_FOUND);
-                        return;
-                    }
-
-                    var storeIds = stores.map((store) => store._id);
-                    var storeItems = [];
-                    var prunedStoreItems = [];
-                    stores.forEach((store) => {
-                        storePickupList.push(store);
-                        storeItems[store._id] = [];
-                    });
-
-                    // Get items that store carries and is on user's shopping list
-                    db.getDB().collection(constants.COLLECTION_STOREHAS).find({
-                        "storeId": { $in: storeIds },
-                        "itemId": { $in: itemIds },
-                        "quantity": { $gt: 0 }
-                    }, {projection: {_id: 0}}).toArray((storeHasItemErr, storeHasItem) => {
-                        storeHasItem.forEach((storeItem, storeId) => {
-                            storeItems[storeItem.storeId].push(storeItem.itemId);
-                        });
-
-                        // Sort store with most items to fewest
-                        var storeItemsSorted = Object.keys(storeItems).map((key) => {
-                            return [key, storeItems[key]];
-                        }).sort((a, b) => {
-                            return b[1].length - a[1].length;
-                        });
-
-                        // Remove redundant items. Apply greedy algorithm so that we check the stores that carry the most items first
-                        var checkedItems = [];
-
-                        storeItemsSorted.forEach((storeEntry) => {
-                            const itemStore = storeEntry[0];
-                            const itemList = storeEntry[1];
-
-                            itemList.forEach((item) => {
-                                if (!checkedItems.includes(item.toString())) {
-                                    checkedItems.push(item.toString());
-                                    if (!prunedStoreItems[itemStore]) {
-                                        prunedStoreItems[itemStore] = [];
-                                    }
-                                    prunedStoreItems[itemStore].push(item);
-                                }
-                            });
-                        });
-
-                        // Add item object to the stores collection objects
-                        for (const itemStore in prunedStoreItems) {
-                            if ({}.hasOwnProperty.call(prunedStoreItems, itemStore)) {
-                                const itemList = prunedStoreItems[itemStore];
-                                itemList.forEach((storeItem) => {
-                                    var itemToAdd = items.filter((item) => item._id.toString() === storeItem.toString())[0];
-                                    var storeHasItemFiltered = storeHasItem.filter((storeHas) => storeHas.storeId.toString() === itemStore.toString() && storeHas.itemId.toString() === itemToAdd._id.toString())[0];
-
-                                    var storeToUpdate = storePickupList.filter((store) => store._id.toString() === itemStore.toString())[0];
-                                    if (!storeToUpdate.items) {
-                                        storeToUpdate.items = [];
-                                    }
-
-                                    itemToAdd.quantity = storeHasItemFiltered.quantity;
-                                    itemToAdd.price = storeHasItemFiltered.price;
-                                    storeToUpdate.items.push(itemToAdd);
-                                });
-                            }
-                        }
-
-                        // Identify stores that we can remove (i.e. the ones with no items to pick up)
-                        var positionsToDelete = [];
-                        storePickupList.forEach((store, key) => {
-                            if (!store.items) {
-                                positionsToDelete.push(key);
-                            }
-                        });
-
-                        // Sort in reverse order to prevent indexing problems during actual deletion
-                        positionsToDelete.sort((a,b) => {
-                            return b - a;
-                        });
-
-                        // Delete the stores identified above
-                        positionsToDelete.forEach((key) => {
-                            storePickupList.splice(key, 1);
-                        });
-
-                        // Send response to client and store in Redis
-                        const response = { stores: storePickupList };
-                        redis.set(redisKey, JSON.stringify(response), () => {});
-                        res.status(constants.RES_OK).send(response);
-                    });
-                });
-            });
-        }
-    });
-    return;
-});
-
+router.post("/feweststores", complexLogic);
 
 // Endpoint for getting nearest stores
 router.post("/nearbyStores", (req, res) => {
